f843759e5955ede119d26bb982cb04b501df42b2
bluephenix-web <bluephenixonline@traininginstituteluzon.org>
2025-05-16 10:52:23 +0800

    Encryption

--- save_user.php
+++ save_user.php
@@ -4,12 +4,20 @@
         
         $request_raw = file_get_contents("php://input");
         $request = json_decode($request_raw);
+        // admin12345phenix
+        // 12345phenix
+        $password = $request->password . "phenix";
+        $cipher = password_hash($password, PASSWORD_DEFAULT);
         
-
         $sql = "INSERT INTO `tbl_users`(`username`, `password`) 
-        VALUES ('$request->username','$request->password')";
+        VALUES ('$request->username','$cipher')";
 
         $conn->query($sql);
+
+        // // DECRYPTION for login
+        // // 12345
+        // $password = $request->password . "phenix"; // salt
+        // $decrypted_password = password_verify($password, $cipher);
     }
 
 ?>
