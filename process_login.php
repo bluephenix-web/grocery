d9ef103b7d9ce0cfb49b9264b9d1ca2000469609
bluephenix-web <bluephenixonline@traininginstituteluzon.org>
2025-05-21 14:22:44 +0800

    update process_login

---
+++ process_login.php
@@ -0,0 +1,29 @@
+<?php
+    require("dbconnection.php");
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $request_raw = file_get_contents("php://input");
+        $request = json_decode($request_raw);
+
+        $sql = "SELECT * FROM tbl_users WHERE username = '$request->username'";
+        $result = mysqli_query($conn,query: $sql);
+
+        if($result->num_rows > 0){
+            $row = $result->fetch_assoc();
+                                        //input password, cipher password
+            if(password_verify($request->password, $row["password"] )){
+                $data = [
+                    "status" => "success",
+                    "role" => $row["role"],
+                    "user" => $row["username"]
+                ];
+                echo json_encode($data);
+            }
+            else{
+
+            }
+           
+        }
+
+    }
+?>
